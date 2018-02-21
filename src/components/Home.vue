56646922d6ebfa95c7ea714de58c7c1c097ad1c6
Simone Taeggi <simone.taeggi@leovegas.com>
2018-02-21 16:32:36 +0100

    integrating Bulma.css

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Faq from '@/components/Faq'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/faq',
+      name: 'Faq',
+      component: Faq
     }
   ]
 })
