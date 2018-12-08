addd0d7f736c16003789dc641a40d196b07276a7
Simone Taeggi <startae14@gmail.com>
2018-12-08 20:29:04 +0100

    Add Hero content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            {{ heading }}
+          </h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">
+              {{ subheading }}
+            </h2>
+          </div>
+          <a href="" id="learn" class="button is-large is-primary">Learn more</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
 }
 </script>
 
@@ -14,4 +34,20 @@ export default {
 <style lang="sass" scoped>
 @import '../mq'
 
+.hero
+  background: url('../assets/clouds.jpg')
+  background-size: cover
+
+  .title
+    font-weight: 300
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
 </style>
