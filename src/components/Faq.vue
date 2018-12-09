1975341e9aaf418616642982fd57f70e5309b0e4
Simone Taeggi <startae14@gmail.com>
2018-12-09 01:17:03 +0100

    add faq view

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,17 +1,64 @@
 <template>
   <div class="faq">
-    FAQ
+    <div class="container">
+      <section class="section">
+        <h1 class="title">
+          FAQ
+        </h1>
+        <h2 class="subtitle is-4">
+          Lorem ipsum and all of that jazz.
+        </h2>
+        <div class="columns" v-if="faqs && faqs.length">
+          <div v-bind:key="faq.id" class="column is-one-third" v-for="faq of faqs">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">
+                  {{ faq.title }}
+                </p>
+                <p class="answer">
+                  {{ faq.body }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'faq'
+  name: 'faq',
+  data: () => ({
+    faqs: [],
+    errors: []
+  }),
+  created () {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.faqs = response.data.slice(0, 10)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import '../mq'
+
+.pd
+  padding: 2.5em 0 1.5em 0
+
+.answer
+  margin-top: 10px !important
+  color: gray
+
+.columns
+  flex-wrap: wrap
 
 </style>
